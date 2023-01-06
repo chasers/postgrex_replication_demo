83cdef47e7a30efc247ed49743cfcaa7dd6835d1
Chase Granberry <chase@logflare.app>
2023-01-06 12:49:16 -0700

    Start adding a Channel to send/receive db events

--- lib/replication_demo_web/endpoint.ex
+++ lib/replication_demo_web/endpoint.ex
@@ -13,6 +13,10 @@ defmodule ReplicationDemoWeb.Endpoint do
   socket "/live", Phoenix.LiveView.Socket,
     websocket: [connect_info: [session: @session_options], compress: true]
 
+  socket "/socket", ReplicationDemoWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest

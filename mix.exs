292f111ec74cc2b31c6e400f2df83705d4569c96
Chase Granberry <chase@logflare.app>
2023-01-06 15:26:29 -0700

    Remove Github workflow log

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ReplicationDemo.MixProject do
   def project do
     [
       app: :replication_demo,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
